96d469c424434f22d74533fac0f77ccb9e6f2e4d
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:41:34 -0800

    Add files via upload

---
+++ Animals.js
@@ -0,0 +1,12 @@
+"use strict";
+//Written by : TAYYABA RAO
+// Date : 18/feb/2024
+//Roll no : 0038841
+//animals
+const animals = ["dog", "cat", "rabbit"];
+//statment about each animals
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet`);
+}
+//result
+console.log("Any of there would make a great pet1 ");
---
+++ Animals.ts
@@ -0,0 +1,14 @@
+//Written by : TAYYABA RAO
+// Date : 18/feb/2024
+//Roll no : 0038841
+
+//animals
+const animals:string[] = ["dog","cat","rabbit"];
+
+//statment about each animals
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet`);
+}
+
+//result
+console.log("Any of there would make a great pet1 ");
